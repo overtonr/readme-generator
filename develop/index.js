b5a8c138a7b8e0478879834c3fb261c3a7cc516e
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-21 13:37:20 -0400

    Questions for user added to array

--- develop/index.js
+++ develop/index.js
@@ -3,7 +3,16 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your gihub username?",
+    "What is your email?",
+    "What is the title of your application?",
+    "What is the description of your application?",
+    "Please enter data for any insallation instructions.",
+    "Please select a license from the following options.",
+    "Please list any contributing collaborators.",
+    "What are the test instructions for this application?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
